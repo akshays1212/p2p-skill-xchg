cbfe8791aa79e2f8d226c40a6ff37b838d1013d7
Akhil Anand <akhilaanand107@gmail.com>
2026-02-20 13:30:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+function Login() {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    alert("Login clicked (frontend test)");
+  };
+
+  return (
+    <div className="auth-container">
+      <h2>Login</h2>
+
+      <form onSubmit={handleLogin}>
+        <input type="email" placeholder="Email" required />
+        <input type="password" placeholder="Password" required />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
